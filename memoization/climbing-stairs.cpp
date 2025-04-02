825bf4035e6e2772971446760c227dd0faae44fb
youssefyousri <engyoussefyousri@gmail.com>
2025-04-02 15:37:26 +0200

    LC-climbing-stairs [Runtime: 0 ms; Memory: 8.6 MB]

--- memoization/climbing-stairs.cpp
+++ memoization/climbing-stairs.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        return n;
+        if(n==0 || n==1)return 1;
+
+       vector< int> v (n+1,0);
+        v [0]=1;
+        v [1]=1;
+        for (int i=2 ; i<=n;i++){
+            v[i]=v[i-1] + v[i-2];
+        }
+        return v[n];
+
+
         
     }
 };
