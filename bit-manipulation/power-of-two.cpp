5d95a991f8b7976c1d31faec8c85bb812a6ed610
youssefyousri <engyoussefyousri@gmail.com>
2025-04-03 17:46:27 +0200

    LC-power-of-two [Runtime: N/A; Memory: N/A]

--- bit-manipulation/power-of-two.cpp
+++ bit-manipulation/power-of-two.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int m ;
-        if(n==1)return true;
-        else if(n%2==0)
+        // return (n > 0) && ((n & (n - 1)) == 0);
+        int m=sqrt(n);
+        if(n==1)return true ;
+     else if(m%2==0)
         return true ;
         else 
         return false;
