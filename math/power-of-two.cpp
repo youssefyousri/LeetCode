7d8b9dd5e1ed4512cda435a042e2b3a118b90a9b
youssefyousri <engyoussefyousri@gmail.com>
2025-04-03 17:47:49 +0200

    LC-power-of-two [Runtime: 0 ms; Memory: 7.7 MB]

--- math/power-of-two.cpp
+++ math/power-of-two.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        // return (n > 0) && ((n & (n - 1)) == 0);
-        int m=sqrt(n);
-        if(n==1)return true ;
-     else if(m%2==0)
-        return true ;
-        else 
-        return false;
+         return (n > 0) && ((n & (n - 1)) == 0);
+
         
     }
 };
