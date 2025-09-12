d2ea3a3322b09d6d2da0ea59348a751cba2abbfb
youssefyousri <engyoussefyousri@gmail.com>
2025-09-12 15:38:32 +0300

    LC-two-sum [Runtime: 35 ms; Memory: 13.8 MB]

--- array/two-sum.cpp
+++ array/two-sum.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    // int size = sizeof(nums) / sizeof(nums[0]);
-    for (int i = 0; i < nums.size(); i++){
-        for (int j = i+1; j < nums.size(); j++){
-            if(target ==(nums[i]+nums[j])){
-               return {i,j};
+        vector<int>result;
+        for(auto it1 = nums.begin();it1 != nums.end();++it1){
+            auto it2 = find(it1+1,nums.end(),target - *it1);
+            if(it2 != nums.end()){
+                result.push_back(it1 - nums.begin());
+                result.push_back(it2 - nums.begin());
+                return result;
             }
         }
-    }
-    return {};
+        return result;
     }
 };
