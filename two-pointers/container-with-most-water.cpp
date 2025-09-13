edafb2c11752e8b772d16f5e87cdcec7cc50a36f
youssefyousri <engyoussefyousri@gmail.com>
2025-09-13 15:21:42 +0300

    LC-container-with-most-water [Runtime: 5 ms; Memory: 63 MB]

--- two-pointers/container-with-most-water.cpp
+++ two-pointers/container-with-most-water.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int min_height = height[0];  
-        int min_height = height[0];       
+        int left = 0;
+        int right = height.size() - 1;
+        int max_area = 0;
+        
+        while (left < right) {
+            int h = min(height[left], height[right]);
+            int w = right - left;
+            max_area = max(max_area, h * w);
+
+            if (height[left] < height[right]) 
+                left++;
+            else 
+                right--;
+        }
+
+        return max_area;
     }
 };
